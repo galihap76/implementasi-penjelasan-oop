6badca45ed4c86f3a8d0de4f9249d8e8eefd4bd6
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-09-23 11:01:42 +0700

    commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+// Mendefinisikan kelas Mobil
+class Mobil {
+    // Deklarasi atribut atau variabel kelas Mobil
+    String merek;
+    boolean menyala = true; // Inisialisasi variabel menyala dengan nilai true
+    boolean tidakMenyala = false; // Inisialisasi variabel tidakMenyala dengan nilai false
+    
+    // Metode startMesin, digunakan untuk memulai mesin mobil
+    void startMesin() {
+        System.out.println("Mesin mobil " + merek + " telah menyala dalam status " + menyala + ".");
+    }
+    
+    // Metode stopMesin, digunakan untuk menghentikan mesin mobil
+    void stopMesin() {
+        System.out.println("Mesin mobil " + merek + " telah berhenti dalam status " + tidakMenyala + ".");
+    }
+}
+
+// Kelas utama Main
+public class Main {
+
+    public static void main(String[] args) {
+        // Membuat objek mobil1 dari kelas Mobil
+        Mobil mobil1 = new Mobil();
+        mobil1.merek = "toyota"; // Menetapkan merek mobil1 menjadi "toyota"
+        mobil1.startMesin(); // Memanggil metode startMesin untuk mobil1
+        
+        // Membuat objek mobil2 dari kelas Mobil
+        Mobil mobil2 = new Mobil();
+        mobil2.merek = "honda"; // Menetapkan merek mobil2 menjadi "honda"
+        mobil2.stopMesin(); // Memanggil metode stopMesin untuk mobil2
+    }
+}
